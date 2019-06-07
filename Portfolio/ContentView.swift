063cc14b79a69e71d6c99f087e409e89b3b52a60
Niklas Sauer <nik.sauer@me.com>
2019-06-07 20:58:11 +0200

    Initial Commit

---
+++ Portfolio/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by Nik Sauer on 07.06.19.
+//  Copyright © 2019 SauerStudios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
